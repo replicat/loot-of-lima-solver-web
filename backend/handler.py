d3509ff1dde251565d462c71fbada773464b90bb
Lucas Lee <replicat@users.noreply.github.com>
2022-05-29 19:07:11 +0800

    demo solver

--- backend/handler.py
+++ backend/handler.py
@@ -1,9 +1,23 @@
 import json
 
+from loot_of_lima_solver.game import StandardGame
+
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def solve(event, context):
+    game = StandardGame()
+    game.solve()
+    body = {
+        "iterations": f"{game._iteration_count}",
+        "solution": f"{game.get_result()}",
         "input": event,
     }
 
